314c48932633a6f1f3b444129eb9db80d74cfc39
laurendukes <laurendukes@google.com>
2019-04-04 15:24:28 -0400

    Unify some y-axis preparations, and make a more descriptive callback for adjustments that happen before the chart configuration gets built.
    Change-Id: I2df78c551766a89e3d60fd1ccd94b39ac798018b
    GitOrigin-RevId: dab1505df2d077bd3c95411027a360397de3eb24

--- medtimeline/src/app/graphtypes/stepgraph/stepgraph.component.ts
+++ medtimeline/src/app/graphtypes/stepgraph/stepgraph.component.ts
@@ -32,27 +32,14 @@ export class StepGraphComponent extends
    * @override
    */
   generateChart() {
-    this.adjustYAxisConfig();
     this.generateBasicChart();
     this.adjustDataDependent();
-
-    if (this.dataPointsInDateRange) {
-      this.chartConfiguration.grid.y = {show: true};
-    }
   }
 
   /**
    * Adjusts the y-axis configuration for the chart.
    */
-  adjustYAxisConfig() {
-    // Give labels to each series and make a map of
-    // x-values to y-values.
-    // Populate the stepgraphcard with data according to c3 format.
-    for (const series of this.data.series) {
-      const label = series.label;
-
-      this.colorsMap[label] = series.legendInfo.fill;
-    }
+  prepareForChartConfiguration() {
     for (const endpointSeries of this.data.endpointSeries) {
       const endpointSeriesId = endpointSeries.label;
       this.data.c3DisplayConfiguration.allColumns.push(
@@ -67,46 +54,24 @@ export class StepGraphComponent extends
       this.colorsMap[endpointSeriesId] = Color.rgb(0, 0, 0);
     }
 
-    // The y-axis configuration for this chart maps each tick on the y-axis,
-    // initially numbers, to discrete labels representing each Medication's
-    // label, and sets the tick values to be those labels.
-    const yValues = Array.from(this.data.yAxisMap.keys());
-    // We need a slightly larger padding for step charts to be aligned with all
-    // other charts.
-    const stepGraphYAxisTickMax = 15;
-    if (this.data.yAxisMap.size === 0) {
-      this.data.yAxisMap.set(10, '');
-    }
-
-    this.yAxisConfig = {
-      // We add the min and max so that when series are hidden by being clicked
-      // on, the y axis does not change and the hidden medications' tick marks
-      // still appear
-      min: 10,
-      max: yValues.sort()[yValues.length - 1],
-      padding: {top: 20, bottom: 20},
-      tick: {
-        // We add padding to our y-axis tick labels so that all y-axes of the
-        // charts rendered on the page can be aligned.
-        // We use an empty string placeholder for each label, so that the axis
-        // does not get shifted over.
-        format: d => {
-          return this.data.yAxisMap.get(d);
-        },
-        values: Array.from(this.data.yAxisMap.keys()),
+    this.yAxisConfig.tick = {
+      // We add padding to our y-axis tick labels so that all y-axes of the
+      // charts rendered on the page can be aligned.
+      // We use an empty string placeholder for each label, so that the axis
+      // does not get shifted over.
+      format: d => {
+        return this.data.yAxisMap.get(d);
       },
+      values: Array.from(this.data.yAxisMap.keys()),
     };
   }
 
-  /**
-   * Adjusts the data-dependent fields of the chart's configuration.
-   */
   adjustDataDependent() {
     // Check if there are any data points in the time range.
     this.dataPointsInDateRange =
         this.data.dataPointsInRange(this.xAxis.dateRange);
-
-    // Don't draw lines between endpoints.
-    this.chartConfiguration.data.types = this.types;
+    if (this.dataPointsInDateRange) {
+      this.chartConfiguration.grid.y = {show: true};
+    }
   }
 }
