30b4edf5a9f7425ecbb499f0be4783e55a60ba4b
Shilpa Kumar <shilpakumar@google.com>
2019-04-18 17:12:20 -0400

    Update some tooltip texts to match test tracker
    Change-Id: I08e26aef5796b9561bc191ff8e10cc52d6f53b83
    GitOrigin-RevId: 467ddcf7f188a3e0da06ec3270a8588a3aba657c

--- medtimeline/src/constants.ts
+++ medtimeline/src/constants.ts
@@ -51,6 +51,7 @@ export const UI_CONSTANTS = {
   // Dialog for adding an event to the custom timeline
   ADD_TITLE_CUSTOM_EVENT: 'Add title',
   ADD_DESCRIPTION_CUSTOM_EVENT: 'Add description',
+  ADD_CARD_HERE: 'Add card here',
   DATE_CUSTOM_EVENT: 'Date',
   TIME_CUSTOM_EVENT: 'Time',
   CANCEL: 'Cancel',
@@ -94,7 +95,8 @@ export const UI_CONSTANTS = {
   CHANGE_DATE_RANGE: 'Change date range',
   MOCK_DATA_BANNER: 'This is synthesized data used for demo purposes only.',
   LAUNCH_TUTORIAL: 'Launch Tutorial',
-  SAVE_TO_NOTE: 'Save a screenshot of this page to a PowerChart clinical note',
+  SAVE_TO_NOTE:
+      'Save a static image of this page to a PowerChart clinical note',
   // Interacting with cards
   REMOVE_THIS_CARD: 'Remove this card',
   CARD_REMOVED: 'Card removed.',
