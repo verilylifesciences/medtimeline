10eb3395a59cff26c5e3bc65738babaf8ab4439f
Lauren Dukes <laurendukes@google.com>
2019-04-25 18:10:24 +0000

    Merge "Swap out IFU images for gifs."
    GitOrigin-RevId: fec94776e5add0a181da5c30c02663e24811b9b4

--- medtimeline/src/typings.d.ts
+++ medtimeline/src/typings.d.ts
@@ -3,5 +3,4 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-/* Type definition for SMART on FHIR client (from assets/fhir-client.min.js) */
-declare var FHIR: any;
+/* Type definition for SMART on FHIR client (from assets/fhir-client.min.js) */declare var FHIR: any;
