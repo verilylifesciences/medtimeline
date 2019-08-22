f7e144cc68326cef9b59a3d7cbb6f3cc8d9bca73
Haiwen Gui <haiwen@google.com>
2019-08-22 22:09:10 +0000

    Merge "Added e2e tests: changed environment from production to mock to allow for more tests"
    GitOrigin-RevId: 5bd1335e0e6dec2f4b079aa02e220b599ad8a8b4

--- medtimeline/src/app/graphtypes/microbio-graph/microbio-graph.component.spec.ts
+++ medtimeline/src/app/graphtypes/microbio-graph/microbio-graph.component.spec.ts
@@ -3,33 +3,84 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {ChartsModule} from 'ng2-charts';
-import {UI_CONSTANTS, UI_CONSTANTS_TOKEN} from 'src/constants';
+import {async, TestBed} from '@angular/core/testing';
+import {UI_CONSTANTS_TOKEN} from 'src/constants';
+import {WHITE} from 'src/app/theme/verily_colors';
 
 import {MicrobioGraphComponent} from './microbio-graph.component';
+import {Inject} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+import {MicrobioGraphData} from 'src/app/graphdatatypes/microbiographdata';
+import {MicrobioReport} from 'src/app/fhir-data-classes/microbio-report';
+import {Interval, DateTime} from 'luxon';
+import {makePartialMicrobioReports, makeMicrobioReports} from '../../test_utils';
+
+class StubMicrobioComponent extends MicrobioGraphComponent {
+  microbioReport: MicrobioReport[];
+
+  constructor(@Inject(UI_CONSTANTS_TOKEN) readonly uiConstants: any,
+              isPartial?: boolean) {
+    super(TestBed.get(DomSanitizer), uiConstants);
+    if (isPartial) {
+      this.microbioReport = makePartialMicrobioReports();
+    } else {
+      this.microbioReport = makeMicrobioReports();
+    }
+    this.data = MicrobioGraphData.fromMicrobioReports(this.microbioReport, this.sanitizer);
+  }
+}
 
 describe('MicrobioGraphComponent', () => {
   let component: MicrobioGraphComponent;
-  let fixture: ComponentFixture<MicrobioGraphComponent>;
+
+  const dateRange = Interval.fromDateTimes(
+    DateTime.utc(2018, 8, 30), DateTime.utc(2018, 9, 22));
 
   beforeEach(async(() => {
-    TestBed
-        .configureTestingModule({
-          declarations: [MicrobioGraphComponent],
-          imports: [ChartsModule],
-          providers: [{provide: UI_CONSTANTS_TOKEN, useValue: UI_CONSTANTS}]
-        })
-        .compileComponents();
+    TestBed.configureTestingModule({}).compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MicrobioGraphComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should create', () => {
+    component = new StubMicrobioComponent(UI_CONSTANTS_TOKEN);
+    component.dateRange = dateRange;
+    expect(component).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display open points on graph if partial status', () => {
+    component = new StubMicrobioComponent(UI_CONSTANTS_TOKEN, true);
+    component.dateRange = dateRange;
+    component.generateChart();
+
+    component.adjustGeneratedChartConfiguration();
+    for (let i = 0; i < component.chartData.length; i++) {
+      const chartjsSeries = component.chartData[i];
+      expect(chartjsSeries.pointBackgroundColor).toEqual([WHITE.rgb().string()]);
+    }
+  });
+
+  it('should display closed points on graph if final status', () => {
+    component = new StubMicrobioComponent(UI_CONSTANTS_TOKEN);
+    component.dateRange = dateRange;
+    component.generateChart();
+
+    component.adjustGeneratedChartConfiguration();
+    for (let i = 0; i < component.chartData.length; i++) {
+      const chartjsSeries = component.chartData[i];
+      // The pointBackgroundColor should not be white if the
+      // points are depicted as closed
+      expect(chartjsSeries.pointBackgroundColor).not.toEqual([WHITE.rgb().string()]);
+    }
+  });
+
+  it('should display triangles on graph if interpretation is positive'
+      + ' and circles if it is normal', () => {
+    component = new StubMicrobioComponent(UI_CONSTANTS_TOKEN, true);
+    component.dateRange = dateRange;
+    component.generateChart();
+
+    component.adjustGeneratedChartConfiguration();
+    // The second report is positive. The first one is normal
+    expect(component.chartData[0].pointStyle).toEqual(['circle']);
+    expect(component.chartData[1].pointStyle).toEqual(['triangle']);
   });
 });
